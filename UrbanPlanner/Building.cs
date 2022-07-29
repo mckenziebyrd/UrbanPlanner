d994fb0eb610987a4e002e9dd6865dc414e2729c
McKenzie Byrd <mckenziebyrd@me.com>
2022-07-29 13:45:25 -0500

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner
+{
+    internal class Building
+    {
+        private string _designer;
+
+        private DateTime _dateConstructed;
+
+        private string _address;
+
+        private string _owner;
+
+
+    }
+}
